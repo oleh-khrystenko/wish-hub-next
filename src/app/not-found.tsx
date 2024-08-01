9a8b12a5e1fc04b16c1e6e2aaa4b112de234ce51
KhOlAn <olegkhristenko@gmail.com>
2024-08-01 08:35:26 +0300

    deleted favicon.ico; fixed not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,12 @@
 export default function NotFound() {
     return (
         <html>
+            <head>
+                <title>Wish Hub</title>
+                <meta name="description" content="Wish Hub - робить ваші мрії реальністю!" />
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+            </head>
+
             <body className="text-center">
                 <h1 className="font-semibold p-10">404 - Page Not Found</h1>
             </body>
