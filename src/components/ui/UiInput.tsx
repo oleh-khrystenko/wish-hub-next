1bd41a8fd1ac6d9675c8dcf01b648414aba8a0dc
KhOlAn <olegkhristenko@gmail.com>
2024-08-05 07:33:49 +0300

    fixed text size in input for mobile

--- src/components/ui/UiInput.tsx
+++ src/components/ui/UiInput.tsx
@@ -60,7 +60,7 @@ const UiInput: FC<IProps> = forwardRef<
                 >
                     {type === 'multiline' ? (
                         <textarea
-                            className="input textarea-scrollbar relative z-20 min-h-20 w-full resize-y appearance-none rounded-md border-0 bg-transparent px-2.5 py-2 text-sm text-zinc-900 outline-none transition-all duration-300 ease-in-out placeholder:text-transparent dark:text-zinc-800 tablet-md:text-base"
+                            className="input textarea-scrollbar relative z-20 min-h-20 w-full resize-y appearance-none rounded-md border-0 bg-transparent px-2.5 py-2 text-base text-zinc-900 outline-none transition-all duration-300 ease-in-out placeholder:text-transparent dark:text-zinc-800"
                             ref={ref as Ref<HTMLTextAreaElement>}
                             id={id}
                             name={name}
@@ -72,7 +72,7 @@ const UiInput: FC<IProps> = forwardRef<
                     ) : (
                         <input
                             className={
-                                'input relative z-20 w-full appearance-none rounded-md border-0 bg-transparent px-2.5 py-2 text-sm text-zinc-900 outline-none transition-all duration-300 ease-in-out placeholder:text-transparent dark:text-zinc-800 tablet-md:text-base' +
+                                'input relative z-20 w-full appearance-none rounded-md border-0 bg-transparent px-2.5 py-2 text-base text-zinc-900 outline-none transition-all duration-300 ease-in-out placeholder:text-transparent dark:text-zinc-800' +
                                 (type === 'password' ? ' pr-8' : '')
                             }
                             ref={ref as Ref<HTMLInputElement>}
