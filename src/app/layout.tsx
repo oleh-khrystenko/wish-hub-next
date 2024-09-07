64f5d840755559a4d49927ac6617b866ec554c81
KhOlAn <olegkhristenko@gmail.com>
2024-09-07 08:22:43 +0300

    added google scripts to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Script from 'next/script';
 import { Mulish } from 'next/font/google';
 import '@/app/[locale]/globals.css';
 
@@ -34,6 +35,15 @@ export default function RootLayout({
     return (
         <html lang={locale}>
             <head>
+                {/* Google Tag Manager */}
+                <Script id="google-tag-manager" strategy="afterInteractive">
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-PKQK5FN3');`}
+                </Script>
+                {/* End Google Tag Manager */}
                 <title>Wish Hub</title>
                 <meta
                     name="description"
@@ -61,6 +71,18 @@ export default function RootLayout({
                 />
                 <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
                 <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-M8TC7LWZTB"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-M8TC7LWZTB');`}
+                </Script>
+                {/* End Google Analytics */}
             </head>
 
             <body
