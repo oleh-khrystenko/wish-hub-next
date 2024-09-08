947e2d067fd0241e57ae8dc0bfb0bed0ad0ef526
KhOlAn <olegkhristenko@gmail.com>
2024-09-08 12:12:13 +0300

    refactored service-worker

--- src/helpers/utils/notification-manager.ts
+++ src/helpers/utils/notification-manager.ts
@@ -8,14 +8,14 @@ export const checkNotificationSubscription = async (userId: IUser['id']) => {
         if (subscription) return;
         await myUserApi.notificationUnsubscribe({ userId });
     } catch (e) {
-        console.error(e);
+        console.error('Error checking notification subscription:', e);
     }
 };
 
 const urlBase64ToUint8Array = (base64String: string) => {
     const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
+        .replace(/-/g, '+')
         .replace(/_/g, '/');
     const rawData = window.atob(base64);
     const outputArray = new Uint8Array(rawData.length);
@@ -30,24 +30,30 @@ export const requestNotificationPermission = async (userId: IUser['id']) => {
         try {
             const permission = await Notification.requestPermission();
             if (permission === 'granted') {
-                if (!process.env.NEXT_PUBLIC_PUBLIC_VAPID_KEY) {
-                    return Promise.reject(
-                        'NEXT_PUBLIC_PUBLIC_VAPID_KEY is not defined.'
-                    );
+                const publicVapidKey = process.env.NEXT_PUBLIC_VAPID_KEY;
+
+                if (!publicVapidKey) {
+                    throw new Error('NEXT_PUBLIC_VAPID_KEY is not defined.');
                 }
 
                 const registration = await navigator.serviceWorker.ready;
                 const subscription: PushSubscription =
                     await registration.pushManager.subscribe({
                         userVisibleOnly: true,
-                        applicationServerKey: urlBase64ToUint8Array(
-                            process.env.NEXT_PUBLIC_PUBLIC_VAPID_KEY
-                        ),
+                        applicationServerKey:
+                            urlBase64ToUint8Array(publicVapidKey),
                     });
+
                 await myUserApi.notificationSubscribe({ userId, subscription });
+            } else {
+                console.warn('Notification permission was not granted.');
             }
         } catch (e) {
-            console.error(e);
+            console.error('Error requesting notification permission:', e);
         }
+    } else {
+        console.error(
+            'Notifications or Service Workers are not supported in this browser.'
+        );
     }
 };
