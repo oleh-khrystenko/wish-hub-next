ff59a3a7d5445407f0c6b8cf24cc969de22dc989
KhOlAn <olegkhristenko@gmail.com>
2024-09-10 19:54:33 +0300

    refactored show popup

--- src/helpers/hocs/OutsideClickHandler.tsx
+++ src/helpers/hocs/OutsideClickHandler.tsx
@@ -12,17 +12,11 @@ const OutsideClickHandler: FC<IProps> = ({ hide, children }) => {
 
     useEffect(() => {
         const handleClickOutside = (e: MouseEvent) => {
-            // console.log('handleClickOutside');
             if (
                 containerRef.current &&
                 !containerRef.current.contains(e.target as Node)
             ) {
-                // console.log('hide');
                 hide();
-                // document.body.style.pointerEvents = 'none';
-                // e.stopImmediatePropagation();
-                // e.preventDefault();
-                // e.stopPropagation();
             }
         };
 
