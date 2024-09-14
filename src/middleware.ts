0128dcaf901f3367d3f3dedb8784182ffbfe512d
KhOlAn <olegkhristenko@gmail.com>
2024-09-14 08:03:02 +0300

    fixed favicon in config matcher; refactored with ELang

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,26 @@
-import createMiddleware from 'next-intl/middleware';
 import { NextResponse, NextRequest } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
+import { ELang } from '@/models/Settings';
 
 const intlMiddleware = createMiddleware({
-    locales: ['uk', 'en', 'ru'],
-    defaultLocale: 'uk',
+    locales: Object.values(ELang), // Використовуємо всі значення з перерахування
+    defaultLocale: ELang.UK,
 });
 
 export default function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
 
+    // Створюємо регулярний вираз на основі перерахування ELang
+    const langRegex = new RegExp(`^/(${Object.values(ELang).join('|')})/`);
+
     // Перевіряємо, чи маршрут уже містить локалізацію
-    const isLocalized = /^\/(uk|en|ru)\//.test(pathname);
+    const isLocalized = langRegex.test(pathname);
 
     // Якщо маршрут не локалізований, редіректимо на локалізовану версію
     if (!isLocalized) {
-        return NextResponse.redirect(new URL(`/uk${pathname}`, request.url));
+        return NextResponse.redirect(
+            new URL(`/${ELang.UK}${pathname}`, request.url)
+        );
     }
 
     // Викликаємо обробник локалізації після редіректу
@@ -22,5 +28,5 @@ export default function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!_next|api|static|favicon.ico).*)'], // Ігноруємо технічні маршрути
+    matcher: ['/((?!_next|api|static|favicon.svg).*)'], // Ігноруємо технічні маршрути
 };
