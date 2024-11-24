3c91b7ad1c6a84eb4fcfc27896650a525698da2b
KhOlAn <olegkhristenko@gmail.com>
2024-11-24 16:43:24 +0200

    added Attention message to rozigrash-bazhan page

--- src/app/[locale]/rozigrash-bazhan/Body.tsx
+++ src/app/[locale]/rozigrash-bazhan/Body.tsx
@@ -48,6 +48,19 @@ const Body: FC = () => {
                 visualPages={breadcrumbsPages}
             />
 
+            <div className="mt-4 bg-rose-500 p-4 text-center text-2xl font-bold text-zinc-800">
+                ⚠️ Увага! Інформація на цій сторінці неактуальна та не
+                відповідає дійсності. Будь ласка, зверніться до головного сайту
+                або служби підтримки для отримання актуальних даних.
+                <br />
+                <br />
+                <div className="h-0.5 w-full bg-zinc-800"></div>
+                <br />
+                ⚠️ Attention! The information on this page is outdated and does
+                not reflect the current reality. Please refer to the main
+                website or contact our support team for up-to-date information.
+            </div>
+
             <section className="mt-6 grid gap-6 px-4 pb-6 tablet-md:gap-8 desktop-xs:grid-cols-11 desktop-sm:px-0 desktop-sm:pb-10">
                 <div className="desktop-xs:col-span-6">
                     {/* title */}
