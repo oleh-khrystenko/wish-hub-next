baf3f42c6e96478980efd225799d91be5c1d0122
KhOlAn <olegkhristenko@gmail.com>
2024-11-24 16:45:30 +0200

    hid link to rozigrash-bazhan page

--- src/components/layouts/footer/Footer.tsx
+++ src/components/layouts/footer/Footer.tsx
@@ -44,10 +44,6 @@ const Footer: FC<IProps> = ({ remove, isWelcome }) => {
             title: allPagesT('reviews'),
         },
         {
-            href: `rozigrash-bazhan${utmParams ? `?${utmParams}` : ''}`,
-            title: allPagesT('rozigrash-bazhan'),
-        },
-        {
             href: `privacy-policy${utmParams ? `?${utmParams}` : ''}`,
             title: allPagesT('privacy_policy'),
         },
